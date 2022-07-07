a29191d3da241ee745361fc6a04ed98df425a583
Ezequiel Rasgido <ezequielrasgido@icloud.com>
2022-07-06 22:00:09 -0300

    Init

---
+++ MovieApp/MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Ezequiel Rasgido on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
